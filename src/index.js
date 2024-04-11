a26a623828726cfa59021d68a13f6d66782a2dfe
Rutvik Gujarati <mrrutvikgujarati0007@gmail.com>
2024-04-11 19:38:06 +0530

    first fix- +0.65%)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
+import Backend from './Backend/Backend';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Backend />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
